8ebb9521741d8b3fc4bf7ce970990cdce3c29e5c
LucasPires50 <luc_50@outlook.com>
2023-09-30 18:43:42 -0300

    chore: 400 error added if image is not sent

--- script_image/background_remover.py
+++ script_image/background_remover.py
@@ -6,11 +6,9 @@ from flask import request,render_template, Blueprint
 
 # Obtém o caminho do diretório atual onde este script está localizado.
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-print(BASE_DIR)
 
 # UPLOAD_FOLDER = 'script_image/static/uploads'
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
-print(UPLOAD_FOLDER)
 # Formatos de imagens permitidos
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg','webp'])
 
@@ -39,13 +37,27 @@ def home():
 @app_backg_rem.route('/remback',methods=['POST'])
 def remback():
     file = request.files['file']
+    if not file:
+        return render_template('status/erro_400.html'), 400
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
     
         file.save(os.path.join(UPLOAD_FOLDER, filename))
         rembg_img_name = f"{filename.split('.')[0]}_rembg.png"
         
-        full_filename = os.path.join(UPLOAD_FOLDER, rembg_img_name)
-        
         remove_background(os.path.join(UPLOAD_FOLDER, filename),os.path.join(UPLOAD_FOLDER, rembg_img_name))
         return render_template('home.html',org_img_name=filename,rembg_img_name=rembg_img_name)
+
+def delete_files_in_folder(pasta):
+    try:
+        for arquivo in os.listdir(pasta):
+            caminho_arquivo = os.path.join(pasta, arquivo)
+            if os.path.isfile(caminho_arquivo):
+                os.remove(caminho_arquivo)
+        print(f"Todos os arquivos em '{pasta}' foram excluídos com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao excluir os arquivos em '{pasta}': {e}")
+
+# Chame a função para deletar arquivos na pasta "static/uploads"
+pasta_uploads = UPLOAD_FOLDER
+delete_files_in_folder(pasta_uploads)
